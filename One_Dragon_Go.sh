f680e8b514eeecdd27bac013aa99601f14214282
路人甲 <jonaszhang91@gmail.com>
2026-05-20 14:27:34 +0800

    修改运行未知

--- One_Dragon_Go.sh
+++ One_Dragon_Go.sh
@@ -11,7 +11,7 @@ check_dependencies() {
 
 do_upgrade_16_6_fast0() {
     echo ">>> 正在启动升级 16.6 fast0（后台运行）..."
-    cd ~ || exit
+    cd /home/menu || exit
     rm -f /home/menu/POS_update.sh
     wget -O /home/menu/POS_update.sh https://github.com/jonaszhang91/update/raw/refs/heads/main/16.6/POS_update.sh
     exec sudo sh /home/menu/POS_update.sh
@@ -21,7 +21,7 @@ do_upgrade_16_6_fast0() {
 
 do_upgrade_16_7_1_fast0() {
     echo ">>> 正在启动升级 16.7.1 fast0（后台运行）..."
-    cd ~ || exit
+    cd /home/menu || exit
     rm -f /home/menu/POS_update.sh
     wget -O /home/menu/POS_update.sh https://github.com/jonaszhang91/update/raw/refs/heads/main/16.7.1/POS_update.sh
     exec sudo sh /home/menu/POS_update.sh
@@ -31,7 +31,7 @@ do_upgrade_16_7_1_fast0() {
 
 do_upgrade_16_7_2_fast0() {
     echo ">>> 正在启动升级 16.7.2 fast0（后台运行）..."
-    cd ~ || exit
+    cd /home/menu || exit
     rm -f /home/menu/POS_update.sh
     wget -O /home/menu/POS_update.sh https://github.com/jonaszhang91/update/raw/refs/heads/main/16.7.2/POS_update.sh
     exec sudo sh /home/menu/POS_update.sh
@@ -41,7 +41,7 @@ do_upgrade_16_7_2_fast0() {
 
 do_upgrade_30_13() {
     echo ">>> 正在启动升级 30.13（后台运行）..."
-    cd ~ || exit
+    cd /home/menu || exit
     rm -f /home/menu/POS_update.sh
     wget --user=baol22 --password="1qaz@WSX6788" http://skymenu.menusifu.com.cn:29120/18030.13/POS_update.sh
     exec sudo sh /home/menu/POS_update.sh
